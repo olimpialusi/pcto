77bcca71192c8726785492951f8c672c02fd84bf
ollapolla <156683003+olimpialusi@users.noreply.github.com>
2024-01-15 16:32:04 +0100

    Add files via upload

---
+++ ex03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a = 3;
+    int b = 1;
+    int somma;
+    
+    somma = a+b;
+    printf("somma: %d \n", somma);
+    printf("somma: %d \n", a+b);
+}
---
+++ ex04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a = 5000;
+    int b = 50;
+
+    printf("somma: %d \n", a+b);
+    return 0;
+
+}
