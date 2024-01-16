1f9b0f7b975310d79a6015892d9f16e046c1a6e4
ollapolla <156683003+olimpialusi@users.noreply.github.com>
2024-01-16 09:46:59 +0100

    Add files via upload

---
+++ ex07.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+    char iniziale;
+
+    printf ("inserisci iniziale del tuo nome: \n");
+    scanf ("%c", &iniziale);
+    printf ("l'iniziale del tuo nome è: %c \n", iniziale);
+    return 0;
+}
