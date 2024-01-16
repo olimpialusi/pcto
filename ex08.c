c969727e2245fe30f12bee6c497a1883e717de08
ollapolla <156683003+olimpialusi@users.noreply.github.com>
2024-01-16 10:00:35 +0100

    Add files via upload

---
+++ ex08.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int anni;
+    printf("inserisci anni jonny: \n");
+    scanf("%d", &anni);
+    printf("gli anni di jonny sono: %d \n", anni);
+    return (0);
+}
