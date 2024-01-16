c72fa0c3a5a0ea74602a6a2ebcab66377d44f848
ollapolla <156683003+olimpialusi@users.noreply.github.com>
2024-01-16 14:24:44 +0100

    Add files via upload

---
+++ ex16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    float divisione;
+    float divisionedue;
+
+    printf ("inserisci un anno: \n");
+    scanf ("%d", &a);
+    divisione = a % 4;
+    divisionedue = a % 100;
+    if ( divisione == 0 ){
+        if (divisionedue != 0)
+        {
+        printf ("l'anno è bisestile \n");
+        } else {
+            printf("l'anno non è bisestile \n");
+        }
+    }
+    else {
+        printf("l'anno non è bisestile \n");
+    }
+
+    return (0);
+
+}
