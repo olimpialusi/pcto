06256fa6aafe449a5713dd8a1d3ff17bba6efa17
ollapolla <156683003+olimpialusi@users.noreply.github.com>
2024-01-18 15:00:16 +0100

    Add files via upload

---
+++ ex37.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    char parola[12] = {'g', 'i', 'a', 'l', 'l', 'o', '\0', 'e', 'v', 'v', 'a', 'i' };
+    printf("il colore preferito di olla è %s \n", parola);
+    return (0);
+}
